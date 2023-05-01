fc2bd84afe33fa1cd017b61eaec80af4ea40ae72
MoatazTabl <moataztabl.m2@gmail.com>
2023-05-01 14:19:21 +0300

    list view with ahadeth numbers and added ahadethpic

--- lib/tabs/ahadeth.dart
+++ lib/tabs/ahadeth.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:islami/ahadethModel.dart';
 
-class AhadethTab extends StatelessWidget {
-  const AhadethTab({Key? key}) : super(key: key);
+class AhadethTab extends StatefulWidget {
+  @override
+  State<AhadethTab> createState() => _AhadethTabState();
+}
+
+class _AhadethTabState extends State<AhadethTab> {
+  List<AhadethModel> allAhadeth = [];
 
   @override
   Widget build(BuildContext context) {
-    loadFile();
-    return Column(
-      children: [],
-    );
+    if (allAhadeth.isEmpty) {
+      loadFile();
+      print(allAhadeth[0].hadethName);
+    }
+    return ListView.separated(
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {
+              Navigator.pushNamed(context, "");
+            },
+            child: Text(
+              allAhadeth[index].hadethName,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Theme.of(context).primaryColor),
+            ),
+          );
+        },
+        separatorBuilder: (context, index) => Divider(
+              height: 1,
+              color: Theme.of(context).primaryColor,
+            ),
+        itemCount: allAhadeth.length);
   }
 
-  void loadFile() async {
+  void loadFile() {
     rootBundle
         .loadString("assets/files/ahadeth/ahadeth.txt")
         .then((ahadethFile) {
@@ -22,11 +46,16 @@ class AhadethTab extends StatelessWidget {
         List<String> singleHadethLine = ahadethcontent[i].trim().split("\n");
 
         String title = singleHadethLine[0];
-
         singleHadethLine.removeAt(0);
 
         List<String> content = singleHadethLine;
+        // print(content);
+
+        AhadethModel ahadethModel = AhadethModel(title, content);
+        print(ahadethModel.hadethName);
+        allAhadeth.add(ahadethModel);
       }
+      setState(() {});
     }).catchError((error) {
       print(error);
     });
